2d9e9a75af908d111bdf19a5fc38d3e1a9876bea
Ricardo Wagner Mori Moreira <meuscursos2891@gmail.com>
2024-12-17 20:09:23 -0300

    Add files via upload

---
+++ Automacao2/main.py
@@ -0,0 +1,51 @@
+import pyautogui  #biblioteca de automação de mouse e teclado
+import time  #tempo
+
+# ==================================================
+
+time.sleep(5)
+
+pyautogui.press('win')
+
+time.sleep(2)
+
+pyautogui.write('chrome')
+
+time.sleep(2)
+
+pyautogui.press('enter')
+
+time.sleep(2)
+
+pyautogui.write('https://studeo.unicesumar.edu.br/#!/access/login')
+
+time.sleep(2)
+
+pyautogui.press('enter')
+
+# ===================================================
+
+time.sleep(2)
+
+pyautogui.hotkey('tab')
+
+time.sleep(2)
+
+pyautogui.hotkey('tab')
+
+time.sleep(2)
+
+pyautogui.press('enter')
+
+# ===================================================
+
+
+
+
+
+
+
+
+
+
+
